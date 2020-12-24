3d61a6422dd7e5e0d4e4e1d40d9b1257c9cd98ea
onsoim <onsoim@gmail.com>
2020-12-25 03:24:00 +0900

    [ADD] AR Parser
    I've added a base of AR Parser.

--- PARSER.py
+++ PARSER.py
@@ -1,5 +1,11 @@
+from Extension.AR import *
+
 class PARSER:
     def __init__(self, raw):
         self.raw = raw
         self.signature = self.checkSignature(raw[ : 0x8 ])
         self.handler = globals()[self.signature](self.raw)
+
+    def checkSignature(self, sig):
+        if (sig == b'!<arch>\n'):
+            return 'AR'

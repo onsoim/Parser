9f84dc0325af9da85db51ef821ba0ba8cb8a75f1
onsoim <onsoim@gmail.com>
2021-08-10 21:36:05 -0700

    [FIX] spend too much time to follow symbolic link
    I've changed the methodology to check the file is exist or not.

--- Type/FOLDER.py
+++ Type/FOLDER.py
@@ -10,6 +10,19 @@ class FOLDER:
 
 
     def getFiles(self, endswith=''):
-        import glob
+        #########################################################################################
+        # import glob
+        #
+        # if endswith: endswith = f'.{endswith}'
+        #
+        # return [f for f in glob.glob(self.root + '**', recursive=True) if f.endswith(endswith)]
+        #
+        #########################################################################################
+
+        # ![pathlib](docs.python.org/3/library/pathlib.html)
+        from pathlib import Path
+
         if endswith: endswith = f'.{endswith}'
-        return [f for f in glob.glob(self.root + '**', recursive=True) if f.endswith(endswith)]
+
+        return [str(f) for f in Path(self.root).glob(f'**/*{endswith}') if f.is_file()]
+
